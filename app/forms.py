b5ea83d8839671d3b5bf1da4fc15bc14a1725fe3
Alex Yakovlev <alexakovlev@gmail.com>
2016-02-26 00:03:24 -0800

    Reformatted signup forms

---
+++ app/forms.py
@@ -0,0 +1,24 @@
+from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField, validators, ValidationError, PasswordField
+from app.models import db, User
+
+
+class SignupForm(Form):
+  firstname = TextField("First name",  [validators.Required("Please enter your first name.")])
+  lastname = TextField("Last name",  [validators.Required("Please enter your last name.")])
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  password = PasswordField('Password', [validators.Required("Please enter a password.")])
+  submit = SubmitField("Create account")
+ 
+  def __init__(self, *args, **kwargs):
+    Form.__init__(self, *args, **kwargs)
+ 
+  def validate(self):
+    if not Form.validate(self):
+      return False
+     
+    user = User.query.filter_by(email = self.email.data.lower()).first()
+    if user:
+      self.email.errors.append("That email is already taken")
+      return False
+    else:
+      return True
--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from flask.ext.mysqldb import MySQL
 from app import app
 
 from app.models import db
+from app.forms import ContactForm, SignupForm
 
 @app.route('/')
 @app.route('/index')
@@ -13,23 +14,30 @@ def index():
 def login():
 	return render_template('login.html')
 	
-@app.route('/register')
-def register():
-	return render_template('register.html')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	form = SignupForm()
 	
-@app.route('/signUp', methods=['POST'])
-def signUp():
-	_name = request.form['inputName']
-	_email = request.form['inputEmail']
-	_password = request.form['inputPassword']
-	if _name and _email and _password:
-		return json.dumps({'html':'<span>All fields good !!</span>'})
-	else:
-		return json.dumps({'html':'<span>Enter the required fields</span>'})
+	if request.method == 'POST':
+    if form.validate() == False:
+		return render_template('signup.html', form=form)
+    else:   
+		return "[1] Create a new user [2] sign in the user [3] redirect to the user's profile"
+   
+	elif request.method == 'GET':
+		return render_template('signup.html', form=form)
+
+# @app.route('/register')
+# def register():
+	# return render_template('register.html')
+	
+# @app.route('/signUp', methods=['POST'])
+# def signUp():
+	# _name = request.form['inputName']
+	# _email = request.form['inputEmail']
+	# _password = request.form['inputPassword']
+	# if _name and _email and _password:
+		# return json.dumps({'html':'<span>All fields good !!</span>'})
+	# else:
+		# return json.dumps({'html':'<span>Enter the required fields</span>'})
 		
-@app.route('/testdb')
-def testdb():
-	if db.session.query("1").from_statement("select 1").all():
-		return 'it works.'
-	else:
-		return 'something is broken.'
