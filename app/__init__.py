8963990738613b25bfdca9d69ac34ec43361244c
Alex Yakovlev <alexakovlev@gmail.com>
2016-02-26 00:25:54 -0800

    csrf

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = 'ayylmao'
 from app import views
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://velvokay:alpine64@mysql.server/velvokay$default'
